cd30afff72d7b778f4af7d31d7fdff21379ad965
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-05-22 06:55:45 +0700

    Clean

--- src/js/service-worker.d.ts
+++
@@ -1,5 +0,0 @@
-declare const cacheName = "D\u1ECBch thu\u1EADt";
-declare const precachedResources: string[];
-declare function precache(): Promise<void>;
-declare function cacheFirst(request: Request): Promise<Response>;
-//# sourceMappingURL=service-worker.d.ts.map
