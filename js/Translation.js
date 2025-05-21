c26d6315a0145f57c5c1ee2b698d1dc87155688a
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-05-21 21:58:42 +0700

    Update Translation.js

--- js/Translation.js
+++ js/Translation.js
@@ -460,7 +460,7 @@ class Translation {
         return systemInstructions;
     }
     doctranslateIoResponsePostprocess(translatedTextWithUuid, textSentenceWithUuid) {
-        translatedTextWithUuid = translatedTextWithUuid.replace('({)\\n', '$1\n').replace(/(\\")?"?(?:(?:\n|\\n)?\})?(\n?(?:`{3})?)$/, '$1"\n}$2').replaceAll(/\n(?! *"(?:insight|rule|translated_string|[a-z0-9]{7,8}#[a-z0-9]{2,3})"|\}(?=\n?(?:`{3})?$))/g, '\\n').replace(/("translated_string": ")([[\s--\n]\S]+)(?=")/v, (_match, p1, p2) => `${p1}${p2.replaceAll(/([^\\])"/g, '$1\\"')}`);
+        translatedTextWithUuid = translatedTextWithUuid.replace('({)\\n', '$1\n').replace(/(\\")?"?(?:(?:\n|\\n)?\})?(\n?(?:`{3})?)$/, '$1"\n}$2').replaceAll(/\n(?! *"(?:insight|rule|translated_string|[a-z0-9]{6,8}#[a-z0-9]{2,3})"|\}(?=\n?(?:`{3})?$))/g, '\\n').replace(/("translated_string": ")([[\s--\n]\S]+)(?=")/v, (_match, p1, p2) => `${p1}${p2.replaceAll(/([^\\])"/g, '$1\\"')}`);
         const jsonMatch = translatedTextWithUuid.match(/(\{[\s\S]+\})/);
         const potentialJsonString = (jsonMatch != null ? jsonMatch[0] : translatedTextWithUuid.replace(/^`{3}json\n/, '').replace(/\n`{3}$/, '')).replace(/insight": "[\s\S]+(?=translated_string": ")/, '');
         if (Utils.isValidJson(potentialJsonString)) {
@@ -473,7 +473,7 @@ class Translation {
                 translatedStringMap = JSON.parse(parsedResult.translated_string);
             }
             else {
-                const translatedStringParts = parsedResult.translated_string.split(/\s*([a-z0-9]{7,8}#[a-z0-9]{2,3}): (?:[a-z0-9]{7,8}#[a-z0-9]{2,3}: )?/).slice(1);
+                const translatedStringParts = parsedResult.translated_string.split(/\s*([a-z0-9]{6,8}#[a-z0-9]{2,3}): (?:[a-z0-9]{6,8}#[a-z0-9]{2,3}: )?/).slice(1);
                 for (let i = 0; i < translatedStringParts.length; i += 2) {
                     translatedStringMap[translatedStringParts[i]] = translatedStringParts[i + 1].replace(/\n+$/, '');
                 }
