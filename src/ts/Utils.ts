54aa2bdf16539f3972eb23aa5558627a611f5f0b
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-06-02 21:15:25 +0700

    Update

--- src/ts/Utils.ts
+++ src/ts/Utils.ts
@@ -1,4 +1,5 @@
 'use strict'
+/* global JSON5 */
 export default class Utils {
   public static readonly CORS_HEADER_PROXY = 'https://cors-header-proxy.itsdhnam.workers.dev/'
   public static has: (prop: string) => boolean = Object.prototype.hasOwnProperty
@@ -7,7 +8,8 @@ export default class Utils {
   }
   public static isValidJson (jsonString: string): boolean {
     try {
-      JSON.parse(jsonString)
+      // @ts-expect-error JSON5
+      JSON5.parse(jsonString)
       return true
     } catch {
       return false
