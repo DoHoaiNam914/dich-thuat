62ea7dda49d8f4a25f7a29fcb33ff32e69a0a2f4
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-06-07 20:13:49 +0700

    Deploy

--- js/Utils.js
+++ js/Utils.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'
 /* global JSON5 */
 class Utils {
-    static clamp(value, min, max) {
-        return Math.min(max, Math.max(min, value));
+  static clamp (value, min, max) {
+    return Math.min(max, Math.max(min, value))
+  }
+
+  static isValidJson (jsonString) {
+    try {
+      // @ts-expect-error JSON5
+      JSON5.parse(jsonString)
+      return true
+    } catch {
+      return false
     }
-    static isValidJson(jsonString) {
-        try {
-            // @ts-expect-error JSON5
-            JSON5.parse(jsonString);
-            return true;
-        }
-        catch {
-            return false;
-        }
-    }
+  }
 }
-Utils.CORS_HEADER_PROXY = 'https://cors-header-proxy.itsdhnam.workers.dev/';
-Utils.has = Object.prototype.hasOwnProperty;
-export default Utils;
+Utils.CORS_HEADER_PROXY = 'https://cors-header-proxy.itsdhnam.workers.dev/'
+Utils.has = Object.prototype.hasOwnProperty
+export default Utils
