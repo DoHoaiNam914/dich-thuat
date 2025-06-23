379052c7f1a4991399c5ab763d3a4fbcdbd47fca
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-06-23 19:07:52 +0000

    Update

--- src/ts/Translation.ts
+++ src/ts/Translation.ts
@@ -635,8 +635,9 @@ class Translation {
         
           if (doesStream) {
             for await (const chunk of completion) {
-                this.responseText = chunk.choices[0].delta.content ?? ''
+                this.responseText += chunk.choices[0].delta.content ?? ''
                 this.translatedText = systemInstruction === SystemInstructions.DOCTRANSLATE_IO ? this.doctranslateIoPostprocess(this.responseText, textSentenceWithUuid) : this.responseText
+                if (this.translatedText.length === 0) continue
                 if (this.abortController.signal.aborted as boolean) return
                 resolve(this.translatedText, this.text, options)
             }
@@ -1194,7 +1195,7 @@ Your output must only contain the translated text and cannot include explanation
         /* eslint-enable camelcase */
         const COMMA_PATTERN = '(?: , |,)'
         const mayCheckComma = uuidAmount === [...translatedString.matchAll(new RegExp(`${COMMA_PATTERN}\\n${UUID_PATTERN}: `, 'g'))].length
-        translatedStringMap = Object.fromEntries([...translatedString.matchAll(new RegExp(`(${UUID_PATTERN}): (.+(?=${mayCheckComma ? COMMA_PATTERN : ''}\\n${UUID_PATTERN}: |$)(?:\\n(?!${UUID_PATTERN}))?)+`, 'g'))].map(element => element.slice(1)))
+        translatedStringMap = Object.fromEntries([...translatedString.matchAll(new RegExp(`(${UUID_PATTERN}): (.+(?=${mayCheckComma ? COMMA_PATTERN : ''}\\n${UUID_PATTERN}: |$)(?:\\n(?!${UUID_PATTERN}: ))?)+`, 'g'))].map(element => element.slice(1)))
       }
       if (Object.keys(translatedStringMap ?? {}).length > 0) {
         return Object.entries(textSentenceWithUuid).map(([first, second]) => parsedResult[first] ?? translatedStringMap[first] ?? (second.replace(/^\s+/, '').length > 0 ? '' : second)).join('\n')
