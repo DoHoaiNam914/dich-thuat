4bd7ba6814790b0711f18ffd0001fb612a502c5a
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-10-14 12:31:09 +0700

    Initial Vite ReactTS

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,23 @@
-// @ts-check
-
-import eslint from '@eslint/js'
+import js from '@eslint/js'
 import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  tseslint.configs.stylistic,
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ['src/ts/**/*.ts'],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        project: './src/ts/tsconfig.json'
-      }
-    }
-  }
-)
+    },
+  },
+])
