26f20f1b3bdd6f97c8a9927a0a789560380b2f0e
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-11-01 23:35:58 +0700

    Update Translation.js

--- js/Translation.js
+++ js/Translation.js
@@ -537,7 +537,7 @@ class Translation {
                       }
                     ]
                   })))
-                : {},
+                : [],
               {
                 role: 'user',
                 parts: [
