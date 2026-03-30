b70c5bd50d8e9fef8c8a24be4dea0aed5732a245
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2026-03-30 13:52:42 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 'use strict'
 /* global $, confirm, fetch, getSelection, localStorage, open, Papa, sessionStorage */
 import Reader from 'https://cdn.jsdelivr.net/gh/DoHoaiNam914/dich-thuat@gh-pages/js/Reader.js'
-import { MODELS, Translation } from './Translation.js'
+import { MODELS, Translation } from 'https://cdn.jsdelivr.net/gh/DoHoaiNam914/dich-thuat@gh-pages/js/Translation.js'
 import Utils from 'https://cdn.jsdelivr.net/gh/DoHoaiNam914/dich-thuat@gh-pages/js/Utils.js'
 const $addWordButton = $('#add-word-button')
 const $apiKeyTexts = $('.api-key-text')
