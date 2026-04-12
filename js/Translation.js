bf4dbbb66f937aac3eb1df20c6ed06b7a485f911
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2026-04-13 00:02:43 +0700

    Update Translation.js

--- js/Translation.js
+++ js/Translation.js
@@ -1791,7 +1791,7 @@ ${this.text}`
         const targetLangCode = SOURCE_LANGUAGE_CODE_MAP[this.destLang] ?? this.destLang
         const sourceLang = LANGUAGE_LABEL_MAP[sourceLangCode] ?? LANGUAGE_MAP.en
         const targetLang = LANGUAGE_LABEL_MAP[targetLangCode]
-        userMessage = `You are a professional ${sourceLang} (${sourceLangCode}) to ${targetLang} (${targetLangCode}) translator. Your goal is to accurately convey the meaning and nuances of the original ${sourceLang} text while adhering to ${targetLang} grammar, vocabulary, and cultural sensitivities.\nProduce only the ${targetLang} translation, without any additional explanations or commentary. Please translate the following ${sourceLang} text into ${targetLang}:\n\n\n${this.text.trim()}`)
+        userMessage = `You are a professional ${sourceLang} (${sourceLangCode}) to ${targetLang} (${targetLangCode}) translator. Your goal is to accurately convey the meaning and nuances of the original ${sourceLang} text while adhering to ${targetLang} grammar, vocabulary, and cultural sensitivities.\nProduce only the ${targetLang} translation, without any additional explanations or commentary. Please translate the following ${sourceLang} text into ${targetLang}:\n\n\n${this.text.trim()}`
         if (isCustomPromptEnabled) { developerInstructions.push(customPrompt) }
         break
       }
