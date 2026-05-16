1a632aaceb7a7920ce86852a7d2b279c28b13f80
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2026-05-17 05:43:45 +0700

    Update Translation.js

--- js/Translation.js
+++ js/Translation.js
@@ -406,7 +406,7 @@ class Translation {
             model: openaiModelId,
             input: [
               ...[...systemInstructions, ...systemInstruction === SystemInstructions.CHATGPT_TRANSLATE ? developerInstructions : []].map(element => ({
-                role: systemInstruction !== SystemInstructions.CHATGPT_TRANSLATE && (isReasoningModel || isReasoningGptFive) ? 'developer' : 'system',
+                role: systemInstruction === SystemInstructions.CHATGPT_TRANSLATE || !isReasoningModel || !isReasoningGptFive ? 'system' : 'deceloper',
                 content: [
                   {
                     type: 'input_text',
